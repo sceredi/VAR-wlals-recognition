817195ba2411ced3d5e0ee1eced33e9355e462c9
sceredi <ceredi.simone.iti@gmail.com>
2024-01-22 14:43:11 +0100

    feat: added video frames utilities

--- app/dataset/video.py
+++ app/dataset/video.py
@@ -1,16 +1,19 @@
 from typing import List
 import os
 import cv2
+import numpy as np
 
 class Video:
     def __init__(self, gloss: str, video_id: str, split: str, frame_start: int, frame_end: int, fps:int, bbox: List[int]) -> None:
         self.gloss = gloss
         self.video_id = video_id
         self.split = split
-        self.frame_start = frame_start
-        self.frame_end = frame_end
         self.fps = fps
         self.bbox = bbox
+        self.frame_start = frame_start
+        if frame_end == -1:
+            frame_end = int(self.get_video_capture().get(cv2.CAP_PROP_FRAME_COUNT))
+        self.frame_end = frame_end
 
     @classmethod
     def from_instance(cls, gloss: str, instance: dict) -> 'Video':
@@ -30,12 +33,27 @@ class Video:
     def __repr__(self) -> str:
         return str(self)
 
+    def is_missing(self) -> bool:
+        return os.path.isfile(self.get_path()) == False
+
     def get_path(self) -> str:
         return f"data/videos/{self.video_id}.mp4"
 
     def get_video_capture(self) -> 'cv2.VideoCapture':
-        return cv2.VideoCapture(self.get_path())
+        if self.video_capture is None:
+            self.video_capture = cv2.VideoCapture(self.get_path())
+        return self.video_capture
     
-    def is_missing(self) -> bool:
-        return os.path.isfile(self.get_path()) == False
+    def get_frame(self, frame_number: int) -> 'np.ndarray':
+        self.get_video_capture().set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+        ret, frame = self.get_video_capture().read()
+        if ret == False:
+            raise Exception(f"Error reading frame {frame_number} from video {self.video_id}")
+        return frame
+    
+    def get_frames(self) -> List['np.ndarray']:
+        ret = []
+        for frame_number in range(self.frame_start, self.frame_end + 1):
+            ret.append(self.get_frame(frame_number))
+        return ret
 
