cdf8811b6b18664e2888f956c2787df28c1196f5
sceredi <ceredi.simone.iti@gmail.com>
2024-02-06 17:08:14 +0100

    feat: updated flow calculation parameters

--- app/flow/calculator.py
+++ app/flow/calculator.py
@@ -3,33 +3,36 @@
 from typing import List
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class FlowCalculator:
     def __init__(
         self,
         frames: List["np.ndarray"],
+        last_frame_index: int,
         params: dict = {
-            "pyr_scale": 0.5,
-            "levels": 3,
-            "winsize": 15,
-            "iterations": 3,
+            "pyr_scale": 0.3,
+            "levels": 5,
+            "winsize": 10,
+            "iterations": 6,
             "poly_n": 5,
-            "poly_sigma": 1.2,
+            "poly_sigma": 1.5,
             "flags": 0,
         },
     ):
         self.frames = frames
+        self.last_frame_index = last_frame_index
         self.params = params
 
-    def calculate(self):
+    def calculate(self, plot_each_frame: bool = False):
         frames = []
         prev_frame = cv2.cvtColor(self.frames[0], cv2.COLOR_BGR2GRAY)
         hsv = np.zeros_like(self.frames[0])
         hsv[..., 1] = 255
         bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
         frames.append(bgr)
-        for frame in self.frames[1:]:
+        for i, frame in enumerate(self.frames[1:self.last_frame_index + 1]):
             next_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             flow = cv2.calcOpticalFlowFarneback(
                 prev_frame, next_frame, None, **self.params
@@ -38,7 +41,27 @@ class FlowCalculator:
             hsv[..., 0] = angle * 180 / np.pi / 2
             hsv[..., 2] = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX)
             bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+            if plot_each_frame:
+                self._plot(frame, bgr, i)
             frames.append(bgr)
             prev_frame = next_frame
+        mask = np.zeros_like(self.frames[0])
+        for _ in range(self.last_frame_index + 1, len(self.frames)):
+            frames.append(mask)
         return frames
+
+    def _plot(self, frame, flow, index):
+        fig, axes = plt.subplots(1, 2)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        axes[0].imshow(frame)
+        axes[0].set_title(f'Frame {index}')
+
+        axes[1].imshow(flow)
+        axes[1].set_title(f'Flow {index}')
+        for ax in axes:
+            ax.set_xticks([])
+            ax.set_yticks([])
+        plt.show()
+
 
