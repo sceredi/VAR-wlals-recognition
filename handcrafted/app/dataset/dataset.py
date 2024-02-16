39fba0a1d7d9e7adf4a3af9262e03d5bd5fd1093
sceredi <ceredi.simone.iti@gmail.com>
2024-02-16 18:44:39 +0100

    feat: changed glosses loading

--- handcrafted/app/dataset/dataset.py
+++ handcrafted/app/dataset/dataset.py
@@ -1,5 +1,5 @@
 import json
-from typing import List
+from typing import List, Tuple
 
 from handcrafted.app.dataset.video import Video
 
@@ -7,20 +7,22 @@ from handcrafted.app.dataset.video import Video
 class Dataset:
     def __init__(self, filename: str) -> None:
         self.filename = filename
-        self.videos = self.load_videos()
+        self.videos, self.glosses = self.load_videos()
 
-    def load_videos(self) -> List[Video]:
+    def load_videos(self) -> Tuple[List[Video], List[str]]:
         with open(self.filename) as f:
             data = json.load(f)
         ret = []
+        glosses = []
         for word_data in data:
             gloss = word_data["gloss"]
+            glosses.append(gloss)
             instances = word_data["instances"]
             for instance in instances:
                 video = Video.from_instance(gloss, instance)
                 if video.is_missing() == False:
                     ret.append(video)
-        return ret
+        return ret, glosses
 
     def update_videos(self) -> None:
         with open(self.filename) as f:
