e03b044d4e55210f43a7d3620cad1b80f85a3bb6
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-02-26 16:15:05 +0100

    chore: add model statistics

---
+++ handcrafted/model/model_statistics.py
@@ -0,0 +1,45 @@
+import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from sklearn.metrics import (accuracy_score, classification_report,
+                             confusion_matrix)
+
+
+class ModelStatistics:
+    def __init__(self, save_dir="plots"):
+        os.makedirs(save_dir, exist_ok=True)
+        self.save_dir = save_dir
+
+    def plot_confusion_matrix(self, y_test, y_pred, save : bool = True):
+        """Plot confusion matrix."""
+        cfm = confusion_matrix(y_test, y_pred)
+        labels = sorted(set(y_test))
+        df_cfm = pd.DataFrame(cfm, index=labels, columns=labels)
+
+        plt.figure(figsize=(10, 7))
+        cfm_plot = sns.heatmap(df_cfm, annot=True, cmap="Blues", fmt="d")
+        plt.xlabel("Predicted")
+        plt.ylabel("Actual")
+        plt.title("Confusion Matrix")
+
+        if save:
+            path = os.path.join(self.save_dir, "confusion_matrix.png")
+            plt.savefig(path)
+            print(f"Confusion matrix saved at {path}")
+
+        plt.show()
+
+    @staticmethod
+    def print_classification_report(y_test, y_pred):
+        """Print classification report."""
+        report = classification_report(y_test, y_pred)
+        print("Classification Report:\n", report)
+
+    @staticmethod
+    def print_accuracy(y_test, y_pred):
+        """Print accuracy of the model."""
+        acc = accuracy_score(y_test, y_pred)
+        print(f"Model Accuracy: {acc:.4f}")
+        return acc
