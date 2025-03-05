8e04c61738ea03f80f65a29929bbe1fe00ac6bab
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-03-05 20:44:48 +0100

    docs: reformat documentation

--- handcrafted/app/extractor/lbp_extractor.py
+++ handcrafted/app/extractor/lbp_extractor.py
@@ -1,3 +1,5 @@
+"""LBPExtractor class for extracting Local Binary Patterns (LBP) features from a list of image frames."""
+
 from typing import List, Tuple
 
 import cv2
@@ -6,16 +8,18 @@ from skimage.feature import local_binary_pattern
 
 
 class LBPExtractor:
-    """Local Binary Patterns class to extract LBP features from a list of frames"""
+    """Local Binary Patterns class to extract LBP features from a list of frames."""
 
     def __init__(self, frames: List[np.ndarray], radius: int = 3) -> None:
-        """
+        """Initialize the LBPExtractor class.
+
         Parameters
         ----------
         frames : List[np.ndarray]
             The frames to extract LBP features from
         radius : int
-            The radius of the LBP circle
+            The radius of the LBP circle.
+
         """
         self._frames = frames
         self._radius = radius
@@ -26,8 +30,7 @@ class LBPExtractor:
 
     @staticmethod
     def _to_gray(frame: np.ndarray) -> np.ndarray:
-        """
-        Converts the image to grayscale
+        """Convert the image to grayscale.
 
         Parameters
         ----------
@@ -37,20 +40,21 @@ class LBPExtractor:
         Returns
         -------
         np.ndarray
-            The grayscale frame
+            The grayscale frame.
+
         """
         if len(frame.shape) == 3:
             return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         return frame
 
     def process_frames(self) -> List[np.ndarray]:
-        """
-        Extracts LBP features from a list of frames
+        """Extract LBP features from a list of frames.
 
         Returns
         -------
         List[np.ndarray]
-            The LBP features
+            The LBP features.
+
         """
         self._lbp_frames, self._features = zip(
             *(self._extract(self._to_gray(frame)) for frame in self._frames),
@@ -62,8 +66,7 @@ class LBPExtractor:
     def _extract(
         self, frame: np.ndarray
     ) -> Tuple[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
-        """
-        Extracts LBP features from a single frame
+        """Extract LBP features from a single frame.
 
         Parameters
         ----------
@@ -73,7 +76,8 @@ class LBPExtractor:
         Returns
         -------
         Tuple[np.ndarray, np.ndarray]
-            The LBP features and the LBP frame
+            The LBP features and the LBP frame.
+
         """
         lbp_frame = local_binary_pattern(
             frame, self._n_points, self._radius, method="uniform"
@@ -89,26 +93,26 @@ class LBPExtractor:
         return lbp_frame, (hist, bins)
 
     def get_lbp_frames(self) -> List[np.ndarray]:
-        """
-        Get the LBP frames
+        """Get the LBP frames.
 
         Returns
         -------
         List[np.ndarray]
-            The LBP frames
+            The LBP frames.
+
         """
         if self._lbp_frames is None:
             self.process_frames()
         return self._lbp_frames
 
     def get_lbp_features(self) -> List[np.ndarray]:
-        """
-        Get the LBP features
+        """Get the LBP features.
 
         Returns
         -------
         List[np.ndarray]
-            The LBP features
+            The LBP features.
+
         """
         if self._features is None:
             self.process_frames()
