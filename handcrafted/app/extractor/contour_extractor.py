f9c4bd206d20d56391a0ffd75c7982a43eeaa6e1
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-03-05 22:25:54 +0100

    docs: reformat documentation

--- handcrafted/app/extractor/contour_extractor.py
+++ handcrafted/app/extractor/contour_extractor.py
@@ -1,3 +1,5 @@
+"""ContourExtractor class for extracting contours from a list of image frames."""
+
 from typing import List
 
 import cv2
@@ -5,21 +7,49 @@ import numpy as np
 
 
 class ContourExtractor:
-    """
-    ContourExtractor class which is used to find contours in a video.
-    """
+    """ContourExtractor class which is used to find contours from frames."""
 
-    def __init__(self, frames: List["np.ndarray"]) -> None:
+    def __init__(self, frames: List[np.ndarray]) -> None:
+        """Initialize the ContourExtractor.
+
+        Parameters
+        ----------
+        frames : List[np.ndarray]
+            The frames to extract contours from.
+
+        """
         self.frames = frames
 
-    def process_frames(self) -> List["np.ndarray"]:
+    def process_frames(self) -> List[np.ndarray]:
+        """Extract contours from a list of frames.
+
+        Returns
+        -------
+        List[np.ndarray]
+            The processed frames with drawn contours.
+
+        """
         frames = []
         for frame in self.frames:
             frame = self._extract(frame)
             frames.append(frame)
         return frames
 
-    def _extract(self, frame: "np.ndarray") -> "np.ndarray":
+    @staticmethod
+    def _extract(frame: np.ndarray) -> np.ndarray:
+        """Extract contours from a single frame.
+
+        Parameters
+        ----------
+        frame : np.ndarray
+            The frame to extract contours from.
+
+        Returns
+        -------
+        np.ndarray
+            The frame with detected contours drawn.
+
+        """
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         blurred_frame = cv2.GaussianBlur(gray_frame, (5, 5), 0)
         _, binary_frame = cv2.threshold(
