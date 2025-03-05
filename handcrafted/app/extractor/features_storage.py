670849c272782512ca0bb26dc05ceb21a379232e
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2025-03-05 23:15:27 +0100

    docs: reformat documentation

--- handcrafted/app/extractor/features_storage.py
+++ handcrafted/app/extractor/features_storage.py
@@ -1,3 +1,5 @@
+"""Module to save and load features using joblib."""
+
 import os
 
 import joblib
@@ -9,12 +11,38 @@ class FeaturesStorage:
 
     @staticmethod
     def save_feature(feature: np.ndarray, file_path: str) -> None:
-        """Save the feature to the specified file path."""
+        """Save the feature to the specified file path.
+
+        Parameters
+        ----------
+        feature: np.ndarray
+            The feature to save.
+        file_path: str
+            The file path to save the feature.
+
+        """
         joblib.dump(feature, file_path)
 
     @staticmethod
     def load_feature(file_path: str) -> np.ndarray:
-        """Load a feature from the specified file path."""
+        """Load a feature from the specified file path.
+
+        Parameters
+        ----------
+        file_path: str
+            The file path to load the feature from.
+
+        Returns
+        -------
+        np.ndarray
+            The loaded feature.
+
+        Raises
+        ------
+        FileNotFoundError
+            If the file_path is not found.
+
+        """
         if not os.path.exists(file_path):
             raise FileNotFoundError(f"Feature file not found: {file_path}")
         return joblib.load(file_path)
