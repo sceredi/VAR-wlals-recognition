c9c322b575a6adb86e31c976e03892155e1e83c7
sceredi <ceredi.simone.iti@gmail.com>
2025-03-07 10:24:56 +0100

    docs: add pickle storage utils documentation

--- wlasl_mediapipe/app/utils/mp/file_utils.py
+++ wlasl_mediapipe/app/utils/mp/file_utils.py
@@ -1,15 +1,39 @@
+"""Helper functions used to save and load pickle files."""
+
 import pickle as pkl
+from typing import List
 
 import numpy as np
 
 
-def save_array(arr, path):
+def save_array(arr: List | np.ndarray, path: str) -> None:
+    """Save a numpy array to a pickle file.
+
+    Parameters
+    ----------
+    arr : List | np.ndarray
+        The array to save.
+    path : str
+        The path to save the array to.
+    """
     file = open(path, "wb")
     pkl.dump(arr, file)
     file.close()
 
 
-def load_array(path):
+def load_array(path: str) -> np.ndarray:
+    """Load a numpy array from a pickle file.
+
+    Parameters
+    ----------
+    path : str
+        The path to load the array from.
+
+    Returns
+    -------
+    np.ndarray
+        The loaded numpy array.
+    """
     file = open(path, "rb")
     arr = pkl.load(file)
     file.close()
